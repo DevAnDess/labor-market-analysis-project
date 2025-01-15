410090cc6bc219342b4d456ef39d3316fc7f959e
AsyaXiao <yfcnz2005.smirnova@yandex.ru>
2025-01-16 00:36:58 +0300

    changes 1

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,23 +2,33 @@ import pandas as pd
 
 
 def process_data(raw_data):
-    # Преобразование JSON в DataFrame
     df = pd.DataFrame(raw_data)
-
-    # Удаление ненужных колонок
+    pd.set_option("display.max_rows", 10)
     columns_to_keep = ["name", "salary", "area", "employer"]
     df = df[columns_to_keep]
 
-    # Обработка пропущенных данных
     df["salary"] = df["salary"].fillna("Не указана")
 
-    # Преобразование словарей в строковое представление
+    def format_salary(salary):
+        if isinstance(salary, dict):
+            min_salary = salary.get("min", "Не указано")
+            max_salary = salary.get("max", "Не указано")
+            return f"{min_salary} - {max_salary}"
+        return salary
+
+    df["salary"] = df["salary"].apply(format_salary)
+
+    def format_employer(employer):
+        if isinstance(employer, dict):
+            return employer.get("name", "Не указано")
+        return employer
+
+    df["employer"] = df["employer"].apply(format_employer)
+
     for column in df.columns:
         df[column] = df[column].apply(lambda x: str(x) if isinstance(x, dict) else x)
 
-    # Удаление дубликатов
+    print(df.head())
     df = df.drop_duplicates()
-    print(df.head())  # Покажет первые 5 строк данных
-    print(df.dtypes)  # Покажет типы данных в каждом столбце
 
     return df
