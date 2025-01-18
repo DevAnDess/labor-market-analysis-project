0fcf96d2402614e58255358d90cc0c3ab53300a3
AsyaXiao <yfcnz2005.smirnova@yandex.ru>
2025-01-18 10:21:57 +0300

    Update grouping in analysis and extend max pages in collection
    Changed the grouping in `analyze_data` from salary to experience for clearer categorization. Increased the maximum pages fetched in data collection from 3 to 5 to improve data coverage.

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,3 +1,3 @@
 def analyze_data(data):
-    grouped_data = data.groupby("salary").size().reset_index(name="Количество вакансий")
+    grouped_data = data.groupby("experience").size().reset_index(name="Количество вакансий")
     return grouped_data
